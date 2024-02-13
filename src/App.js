a2183571dfc11bcc203f2a0679b285b734011673
simonmob <kmaingi.simon@gmail.com>
2024-02-13 18:33:39 +0300

    feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello There, Im learning docker
+          Hello There, Im learning docker and AWS
         </p>
         <a
           className="App-link"
